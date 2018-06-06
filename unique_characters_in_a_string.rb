b7c32017d48cabd530f0033999595a1fda55d2d4
nurarenke <nurarenke@gmail.com>
2018-06-06 09:30:33 -0700

    rubydoctest

---
+++ unique_characters_in_a_string.rb
@@ -0,0 +1,12 @@
+=begin
+Unique Characters in a string from cracking the coding interview, problem 1.1
+Implement an algorithm to determine if a string has all unique characters. What if
+you cannot use additional data structures?
+=end
+# >> unique_char
+# => 'testing works'
+
+
+def unique_char
+	'testing works'
+end
